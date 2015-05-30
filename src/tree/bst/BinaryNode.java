c1655dcbd57348de46499aa091dbd5f79072a337
jags4bitbucket <JatinPatel@Jatinkumars-MacBook-Pro.local>
2015-05-30 11:52:36 -0700

    Height of Binary tree

--- src/tree/bst/BinaryNode.java
+++ src/tree/bst/BinaryNode.java
@@ -3,13 +3,20 @@ package tree.bst;
 public class BinaryNode {
 
 	Comparable element;
-	BinaryNode left_node, right_node;
+	public BinaryNode left_node, right_node;
 
 	public BinaryNode(Comparable element) {
 		super();
 		this.element = element;
 		this.left_node = null;
 		this.right_node = null;
+	}
+	
+	public BinaryNode(Comparable element, BinaryNode left, BinaryNode right) {
+		super();
+		this.element = element;
+		this.left_node = left;
+		this.right_node = right;
 	}
 
 }
