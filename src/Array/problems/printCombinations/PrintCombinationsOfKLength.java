36a213ac1cc2993380bba567a41a3e6e3abf5f1d
jgreenrise <jatindpatel@live.com>
2015-07-15 15:43:55 -0700

    Update PrintCombinationsOfKLength.java

--- src/Array/problems/printCombinations/PrintCombinationsOfKLength.java
+++ src/Array/problems/printCombinations/PrintCombinationsOfKLength.java
@@ -16,6 +16,7 @@ public class PrintCombinationsOfKLength {
 
 	public static void main(String[] args) {
 		possibleStrings(3, "abc".toCharArray(), "");
+		possibleStrings_usingBinaryRepresentation(3, "abc".toCharArray(), "");
 	}
 
 	public static void possibleStrings(int maxLength, char[] alphabet,
@@ -35,5 +36,32 @@ public class PrintCombinationsOfKLength {
 				curr = oldCurr;
 			}
 		}
+	}
+	
+	/**
+	 * Limitation for this approach: Will work only for alphabet with max 2
+	 * characters. Since we are dealing with binary representation of string,
+	 * which has 0 and 1 only
+	 */
+	private static void possibleStrings_usingBinaryRepresentation(int i, char[] alphabet, String string) {
+		// TODO Auto-generated method stub
+
+		for (int j = 0; j < Math.pow(2, i); j++) {
+			String value = Integer.toBinaryString(j);
+			String response = "";
+
+			while (value.length() < i) {
+				value = "0" + value;
+			}
+
+			for (int k = 0; k < value.length(); k++) {
+				int index = Integer.parseInt(value.substring(k, k + 1));
+				response = response + alphabet[index];
+			}
+
+			System.out.println(value + " " + response);
+
+		}
+
 	}
 }
