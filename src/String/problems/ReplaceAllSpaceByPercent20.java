b918b4c8fd4d83e24dba72e7982eaccbc8d53fff
jags4bitbucket <jatindpatel@live.com>
2019-09-05 22:54:02 -0700

    Update code

--- src/String/problems/ReplaceAllSpaceByPercent20.java
+++ src/String/problems/ReplaceAllSpaceByPercent20.java
@@ -3,30 +3,31 @@ package String.problems;
 public class ReplaceAllSpaceByPercent20 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String input = "Mr  John Smith     ";
 
-		String input = "i am in love";
-
-		// Approach 1
-		System.out.println(input.replaceAll(" ", "'%20'"));
-
-		// Approach 2 usign string tokenizer
 		System.out.println(replaceUsingStringBuilder(input));
 
 	}
 
 	private static String replaceUsingStringBuilder(String input) {
-		// TODO Auto-generated method stub
 
 		StringBuilder response = new StringBuilder();
+		int counter = 0;
 
-		String[] str_arr_input = input.split(" ");
+		for (int i = 0; i< input.length(); i++){
 
-		for (String item : str_arr_input) {
-			response.append(item + "'%20'");
+			if(counter == 1 && input.charAt(i) == " ".charAt(0))
+				continue;
+
+			if(input.charAt(i) != " ".charAt(0)){
+				response.append(input.charAt(i));
+				counter = 0;
+			}else{
+				response.append("%20");
+				counter = 1;
+			}
+
 		}
-
-		response.delete(response.length() - 5, response.length());
 
 		return response.toString();
 	}
