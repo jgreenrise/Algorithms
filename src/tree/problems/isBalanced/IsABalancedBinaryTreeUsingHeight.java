72a7ff9d750cd185ad6d79790418d75a77f0c335
jags4bitbucket <jatindpatel@live.com>
2019-10-04 18:39:21 -0700

    Adj MATRIX

--- src/tree/problems/isBalanced/IsABalancedBinaryTreeUsingHeight.java
+++ src/tree/problems/isBalanced/IsABalancedBinaryTreeUsingHeight.java
@@ -7,61 +7,53 @@ import tree.bst.BinaryNode;
  */
 public class IsABalancedBinaryTreeUsingHeight {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		BinaryNode n7 = new BinaryNode(7);
-		BinaryNode n8 = new BinaryNode(8);
-		BinaryNode n4 = new BinaryNode(4, n7, n8);
-		BinaryNode n6 = new BinaryNode(6);
-		BinaryNode n5 = new BinaryNode(5);
-		BinaryNode n2 = new BinaryNode(2, n5, n6);
-		BinaryNode n1 = new BinaryNode(1, n2, n4);
+        BinaryNode n7 = new BinaryNode(7);
+        BinaryNode n8 = new BinaryNode(8);
+        BinaryNode n4 = new BinaryNode(4, n7, n8);
+        BinaryNode n6 = new BinaryNode(6);
+        BinaryNode n5 = new BinaryNode(5);
+        BinaryNode n2 = new BinaryNode(2, n5, n6);
+        BinaryNode n1 = new BinaryNode(1, n2, n4);
 
-		System.out.println(isBalancedBinaryTree(n1));
-		
-		BinaryNode n10 = new BinaryNode(10);
-		BinaryNode n11 = new BinaryNode(11);
-		n6.left_node = n10;
-		n10.left_node = n11;
-		
-		System.out.println(isBalancedBinaryTree(n1));
-		
-		n10.left_node = null;
-		
-		System.out.println(isBalancedBinaryTree(n1));
+        System.out.println(isBalancedBinaryTree(n1));
 
-	}
+        BinaryNode n10 = new BinaryNode(10);
+        BinaryNode n11 = new BinaryNode(11);
+        n6.left_node = n10;
+        n10.left_node = n11;
 
-	private static boolean isBalancedBinaryTree(BinaryNode node){
-		
-		int left = height(node.left_node);
-		int right = height(node.right_node);
+        System.out.println(isBalancedBinaryTree(n1));
 
-		if(left > right){
-			if((left - right) > 1)
-				return false;
-			else
-				return true;
-		}else{
-			if((right - left) > 1)
-				return false;
-			else
-				return true;
-		}
+        n10.left_node = null;
 
-	}
+        System.out.println(isBalancedBinaryTree(n1));
 
-	private static int height(BinaryNode node){
-		
-		if(node == null){
-			return 0;
-		}
+    }
 
-		int left = height(node.left_node);
-		int right = height(node.right_node);
+    private static boolean isBalancedBinaryTree(BinaryNode node) {
 
-		return 1 + Integer.max(left, right);
+        int left = height(node.left_node);
+        int right = height(node.right_node);
 
-	}
+        if (Math.abs(left - right) > 1)
+            return false;
+        return true;
+
+    }
+
+    private static int height(BinaryNode node) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        int left = height(node.left_node);
+        int right = height(node.right_node);
+
+        return 1 + Math.max(left, right);
+
+    }
 
 }
