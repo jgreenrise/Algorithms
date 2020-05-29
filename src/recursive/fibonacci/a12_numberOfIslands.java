376cca8a0a6e8d59ba501dec4797ab343f042300
jags4bitbucket <jatindpatel@live.com>
2020-05-28 20:32:09 -0700

    Missging number

--- src/recursive/fibonacci/a12_numberOfIslands.java
+++ src/recursive/fibonacci/a12_numberOfIslands.java
@@ -5,53 +5,54 @@ import java.util.Set;
 
 public class a12_numberOfIslands {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-		//char[][] matrix = { {1,1,0,1}, {0,0,1,0}, {0,0,0,0} };
-		char[][] matrix = { {1,1,1,1,0},{1,1,0,1,0},{1,1,0,0,0},{0,0,0,0,0} };
+        //char[][] matrix = { {1,1,0,1}, {0,0,1,0}, {0,0,0,0} };
+        //char[][] matrix = { {1,1,1,1,0},{1,1,0,1,0},{1,1,0,0,0},{0,0,0,0,0} };
+        char[][] matrix = {{1, 1, 0, 0, 0}, {1, 1, 0, 0, 0}, {0, 0, 1, 1, 0}, {0, 0, 0, 1, 1}};
 
-		a12_numberOfIslands class1 = new a12_numberOfIslands();
-		System.out.println("\n Total island: "+ class1.numIslands(matrix));
+        a12_numberOfIslands class1 = new a12_numberOfIslands();
+        System.out.println("\n Total island: " + class1.numIslands(matrix));
 
-	}
+    }
 
-	public int numIslands(char[][] grid) {
+    public int numIslands(char[][] grid) {
 
-		int totRows = grid.length;
-		int totCols = grid[0].length;
-		int islands = 2;
+        int totRows = grid.length;
+        int totCols = grid[0].length;
+        int islands = 2;
 
-		for(int row=0; row<totRows; row++){
-			for(int col=0; col<totCols; col++){
-				if(grid[row][col] == 0 || grid[row][col] > 1){
-					continue;
-				}
-				islands = dfs(grid, row, col, totRows, totCols, islands) + 1;
-			}
-		}
+        for (int row = 0; row < totRows; row++) {
+            for (int col = 0; col < totCols; col++) {
+                if (grid[row][col] == 0 || grid[row][col] > 1) {
+                    continue;
+                }
+                islands = dfs(grid, row, col, totRows, totCols, islands) + 1;
+            }
+        }
 
-		return islands-2;
-	}
+        return islands - 1;
+    }
 
-	public int dfs(char [][] grid, int sr, int sc, int totRows, int totCols, int islands){
+    public int dfs(char[][] grid, int sr, int sc, int totRows, int totCols, int islands) {
 
-		if(sr >= totRows || sc >= totCols){
-			return islands;
-		}
+        if (sr >= totRows || sc >= totCols) {
+            return islands;
+        }
 
-		if(grid[sr][sc] == 0){
-			return islands;
-		}
+        if (grid[sr][sc] == 0) {
+            return islands;
+        }
 
-		if(grid[sr][sc] == 1){
-			grid[sr][sc] = (char) islands;
-			dfs(grid, sr+1, sc, totRows, totCols, islands);
-			dfs(grid, sr, sc+1, totRows, totCols, islands);
-		}
+        if (grid[sr][sc] == 1) {
+            grid[sr][sc] = (char) islands;
+            dfs(grid, sr + 1, sc, totRows, totCols, islands);
+            dfs(grid, sr, sc + 1, totRows, totCols, islands);
+        }
 
-		return islands;
+        return islands;
 
-	}
+    }
 
 }
