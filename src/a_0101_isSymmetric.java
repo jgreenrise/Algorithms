c30afb8c7ffad9b9efdfa83233b629c6ef59cb46
Jatin Patel <mazabrain@gmail.com>
2024-01-08 12:00:39 -0800

    230 Kth Smallest element in binary tree

--- src/a_0101_isSymmetric.java
+++ src/a_0101_isSymmetric.java
@@ -1,30 +1,29 @@
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
-public class a_100_SameTree {
+public class a_0101_isSymmetric {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
-        if(p == null && q == null)  return true;
-        if(p == null && q != null)  return false;
-        if(p != null && q == null)  return false;
+        if (p == null && q == null) return true;
+        if (p == null && q != null) return false;
+        if (p != null && q == null) return false;
 
-        if(p.val == q.val){
+        if (p.val == q.val) {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        }else{
+        } else {
             return false;
         }
 
@@ -35,7 +34,8 @@ public class a_100_SameTree {
         TreeNode left;
         TreeNode right;
 
-        TreeNode() {}
+        TreeNode() {
+        }
 
         TreeNode(int val) {
             this.val = val;
--- src/a_0392_IsSubSequence.java
+++ src/a_0392_IsSubSequence.java
@@ -2,19 +2,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class a_392_IsSubSequence {
+public class a_0392_IsSubSequence {
     public boolean isSubsequence(String s, String t) {
         return isSubsequence(s, t, 0, 0);
     }
 
-    public boolean isSubsequence(String s, String t, int lIdx, int rIdx){
+    public boolean isSubsequence(String s, String t, int lIdx, int rIdx) {
 
-        if(lIdx >= s.length()) return true;
-        if(rIdx >= t.length()) return false;
+        if (lIdx >= s.length()) return true;
+        if (rIdx >= t.length()) return false;
 
-        if(s.charAt(lIdx) == t.charAt(rIdx)){
+        if (s.charAt(lIdx) == t.charAt(rIdx)) {
             return isSubsequence(s, t, lIdx + 1, rIdx + 1);
-        }else{
+        } else {
             return isSubsequence(s, t, lIdx, rIdx + 1);
         }
     }
