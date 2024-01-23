b27ce6f3f107bce9d07d9e00161c3f1fb4c7b782
Jatin Patel <mazabrain@gmail.com>
2024-01-23 05:39:26 -0800

    285 Inorder successor

--- src-markdown/Tree/Heap.md
+++ src-markdown/Tree/Heap.md
@@ -2,6 +2,8 @@
 
 ## Heap - Array Representation
 
+Reference: (https://www.slideshare.net/geheri12345/maxheappptx)
+
 ![Min Heap](https://miro.medium.com/v2/resize:fit:665/0*zcVbYPDgX_br8ayR.png)
 
 ## Insert Min Heap
--- src/Array/heap/MaxHeap.java
+++ src/Array/heap/MaxHeap.java
@@ -4,47 +4,57 @@ import java.util.Arrays;
 
 public class MaxHeap {
 
-	public static void main(String[] args) {
-		int[] arr_i_input = { 1,4,7,3,8,5 };
-		System.out.println(Arrays.toString(arr_i_input));
-		buildHeap(arr_i_input);
-	}
+    public static void main(String[] args) {
+        int[] arr_i_input = {1, 4, 7, 3, 8, 5};
+        System.out.println(Arrays.toString(arr_i_input));
+        buildHeap(arr_i_input);
+    }
 
-	private static void buildHeap(int[] arr_i_input) {
-		int []  oArr = new int [arr_i_input.length];
-		for (int i = 0; i< arr_i_input.length; i++){
-			oArr = buildMaxHeap(arr_i_input[i], oArr, i);
-		}
-		System.out.println(Arrays.toString(oArr));
-	}
+    private static void buildHeap(int[] arr_i_input) {
+        int[] oArr = new int[arr_i_input.length];
+        for (int i = 0; i < arr_i_input.length; i++) {
+            oArr = buildMaxHeap(arr_i_input[i], oArr, i);
+        }
+        System.out.println(Arrays.toString(oArr));
+    }
 
-	private static int[] buildMaxHeap(int i, int[] oArr, int currIndex) {
-		if(currIndex == 0){
-			oArr[currIndex] = i;
-			return oArr;
-		}else{
-			oArr[currIndex] = i;
-			if(oArr[currIndex] > oArr[(currIndex-1)/2])
-				oArr = percolateUp(currIndex, oArr);
-			return oArr;
-		}
-	}
+    private static int[] buildMaxHeap(int i, int[] oArr, int currIndex) {
+        if (currIndex == 0) {
+            oArr[currIndex] = i;
+            return oArr;
+        } else {
+            oArr[currIndex] = i;
+            if (oArr[currIndex] > oArr[(currIndex - 1) / 2])
+                oArr = percolateUp(currIndex, oArr);
+            return oArr;
+        }
+    }
 
-	private static int[] percolateUp(int currIndex, int[] oArr) {
-		if(oArr[currIndex] > oArr[(currIndex-1)/2]){
-			oArr = swap(currIndex, (currIndex-1)/2, oArr);
-			if ((currIndex-1)/2 == 0) return oArr;
-			else percolateUp((currIndex-1)/2, oArr);
-		}
-		return oArr;
-	}
+    private static int[] percolateUp(int currIdx, int[] nums) {
+        if (nums[currIdx] > nums[(currIdx - 1) / 2]) {
+            nums = swap(currIdx, (currIdx - 1) / 2, nums);
+            upgradeChildNodes(currIdx, nums);
+            if ((currIdx - 1) / 2 == 0) return nums;
+            else percolateUp((currIdx - 1) / 2, nums);
+        }
+        return nums;
+    }
 
-	private static int[] swap(int currIndex, int parentIndex, int[] oArr) {
-		int tmp = oArr[currIndex];
-		oArr[currIndex] = oArr[parentIndex];
-		oArr[parentIndex] = tmp;
-		return oArr;
-	}
+    /**
+     * Incomplete code
+     **/
+    private static void upgradeChildNodes(int currIdx, int[] nums) {
+        int length = nums.length;
+        int leftChildIdx = (2 * currIdx + 1) < nums.length ? nums[2 * currIdx + 1] : -1;
+        int rightChildIdx = (2 * currIdx + 2) < nums.length ? nums[2 * currIdx + 2] : -1;
+    }
+
+    private static int[] swap(int currIndex, int parentIndex, int[] oArr) {
+        int tmp = oArr[currIndex];
+        oArr[currIndex] = oArr[parentIndex];
+        oArr[parentIndex] = tmp;
+        return oArr;
+    }
 
 
 }
