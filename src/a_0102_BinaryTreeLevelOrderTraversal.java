1c154626dd32b8775219509f82305f64bcca0fc3
Jatin Patel <mazabrain@gmail.com>
2024-03-17 21:18:30 -0700

    102. Binary Tree Level Order Traversal

--- src/a_0102_BinaryTreeLevelOrderTraversal.java
+++ src/a_0102_BinaryTreeLevelOrderTraversal.java
@@ -49,6 +49,22 @@ public class a_0102_BinaryTreeLevelOrderTraversal {
 
     }
 
+    public List<List<Integer>> levelOrderRecursion(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        levelHelper(res, root, 0);
+        return res;
+    }
+
+    public void levelHelper(List<List<Integer>> res, TreeNode root, int height) {
+        if (root == null) return;
+        if (height >= res.size()) {
+            res.add(new LinkedList<Integer>());
+        }
+        res.get(height).add(root.val);
+        levelHelper(res, root.left, height + 1);
+        levelHelper(res, root.right, height + 1);
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
