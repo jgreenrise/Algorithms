28bdeeb06562a93012c0e6d15407be4a7c13d078
Jatin Patel <mazabrain@gmail.com>
2024-03-17 22:11:39 -0700

    98. is binary search tree

--- src/a_0098_cci_4_5_IsBST.java
+++ src/a_0098_cci_4_5_IsBST.java
@@ -4,101 +4,34 @@ public class a_0098_cci_4_5_IsBST {
 
     static TreeNode rootNode;
 
-    public static boolean isValidBST(TreeNode root) {
-        return isValidBSTUsingMinMaxApproach(root, null, null);
-
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
     }
 
-    private static boolean isValidBSTUsingMinMaxApproach(TreeNode node, Long min, Long max) {
+    public boolean isValidBST(TreeNode node, Integer min, Integer max) {
 
-        if (node == null)
-            return true;
+        if (node == null) return true;
 
-        if (min != null && node.val <= min) {
-            return false;
-        }
-
-        if (max != null && node.val >= max) {
-            return false;
-        }
-
-
-        return isValidBSTUsingMinMaxApproach(node.left, min, (long) (node.val)) &&
-                isValidBSTUsingMinMaxApproach(node.right, (long) (node.val), max);
-    }
-
-    public boolean isValidBSTv1(TreeNode root) {
-
-        ModifiedTreeNode upgradedRoot = isValidBinaryTree(root);
-        return upgradedRoot.isBST;
-    }
-
-    public ModifiedTreeNode isValidBinaryTree(TreeNode node) {
-
-        if (node == null)
-            return null;
-
-        ModifiedTreeNode newnode = new ModifiedTreeNode(node.val);
-        newnode.left = isValidBinaryTree(node.left);
-        newnode.right = isValidBinaryTree(node.right);
-
-        if (newnode.left != null && newnode.right != null) {
-            if (!newnode.left.isBST || !newnode.right.isBST) {
-                newnode.isBST = false;
-            } else if ((newnode.val > newnode.left.val && newnode.val < newnode.right.val) &&
-                    (newnode.val > (newnode.left).max_so_far && newnode.val < (newnode.right).min_so_far)) {
-                newnode.min_so_far = newnode.left.min_so_far;
-                newnode.max_so_far = newnode.right.max_so_far;
-                newnode.isBST = true;
+        if (node.left != null) {
+            if (node.left.val < node.val) {
+                if (min != null && node.left.val <= min) return false;
+                if (max != null && node.left.val >= max) return false;
             } else {
-                newnode.isBST = false;
+                return false;
             }
-            return newnode;
-        } else if (newnode.left == null && newnode.right != null) {
-            newnode.min_so_far = newnode.val;
-            if (!newnode.right.isBST) {
-                newnode.isBST = false;
-            } else if (newnode.val < newnode.right.val &&
-                    (newnode.val < newnode.right.min_so_far)) {
-                newnode.max_so_far = newnode.right.max_so_far;
-                newnode.isBST = true;
-            } else {
-                newnode.isBST = false;
-            }
-            return newnode;
-
-        } else if (newnode.left != null && newnode.right == null) {
-            newnode.max_so_far = newnode.val;
-            if (!newnode.left.isBST) {
-                newnode.isBST = false;
-            } else if (newnode.val > newnode.left.val &&
-                    (newnode.val > newnode.left.max_so_far)) {
-                newnode.min_so_far = newnode.left.min_so_far;
-                newnode.isBST = true;
-            } else {
-                newnode.isBST = false;
-            }
-            return newnode;
-        } else {
-            newnode.min_so_far = newnode.val;
-            newnode.max_so_far = newnode.val;
-            newnode.isBST = true;
-            return newnode;
         }
 
-    }
-
-    public class ModifiedTreeNode extends TreeNode {
-        public int min_so_far;
-        public int max_so_far;
-        public ModifiedTreeNode left;
-        public ModifiedTreeNode right;
-        public boolean isBST = false;
-
-        public ModifiedTreeNode(int val) {
-            super(val);
+        if (node.right != null) {
+            if (node.right.val > node.val) {
+                if (min != null && node.right.val <= min) return false;
+                if (max != null && node.right.val >= max) return false;
+            } else {
+                return false;
+            }
         }
 
+        return isValidBST(node.left, min, node.val) &&
+                isValidBST(node.right, node.val, max);
     }
 
 }
