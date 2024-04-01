0bd4916ecab527083f906b797762f626f16d49eb
Jatin Patel <mazabrain@gmail.com>
2024-03-31 21:13:02 -0700

    14 LongestCommon Prefix

--- src/a_0014_longestCommonPrefix.java
+++ src/a_0014_longestCommonPrefix.java
@@ -1,5 +1,38 @@
 // https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=top-interview-150
+
+import java.util.Arrays;
+
+// Effeicnt solution using sorting
 public class a_0014_longestCommonPrefix {
+
+    public String longestCommonPrefixUsingSorting(String[] strs) {
+
+        if (strs.length == 1) return strs[0];
+        Arrays.sort(strs);
+        int i = 0;
+        int j = strs.length - 1;
+
+        while (i != j) {
+            int minPrefixLen = 0;
+            if ((strs[i]).length() > 0 && strs[i].charAt(0) == strs[j].charAt(0)) {
+                minPrefixLen = 1;
+                char[] leftChars = (strs[i]).toCharArray();
+                char[] rightChars = (strs[j]).toCharArray();
+                int k = 1;
+                while (k < leftChars.length && leftChars[k] == rightChars[k]) {
+                    minPrefixLen++;
+                    k++;
+                }
+                String originalString = new String(leftChars);
+                return originalString.substring(0, minPrefixLen);
+            } else {
+                return "";
+            }
+        }
+
+        return "";
+
+    }
 
     public String longestCommonPrefix(String[] strs) {
 
@@ -27,12 +60,11 @@ public class a_0014_longestCommonPrefix {
                 }
                 minPrefixLen = Math.min(minPrefixLen, currLen);
             }
-            currCharArr = null;
         }
 
         StringBuilder sbr = new StringBuilder();
         for (int j = 0; j < minPrefixLen; j++)
-            sbr.append(String.valueOf(chars[j]));
+            sbr.append(chars[j]);
 
         //String originalString = new String(chars);
         //return originalString.substring(0, maxPrefixLen);
