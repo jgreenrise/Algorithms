defc998bd3c03edf98ee77dfcd444c70a6fd885f
Jatin Patel <mazabrain@gmail.com>
2024-10-06 10:53:00 -0700

    1385 find the distance

--- src/a_1385_FindDistanceBetweenArrays.java
+++ src/a_1385_FindDistanceBetweenArrays.java
@@ -3,41 +3,36 @@ import java.util.Arrays;
 public class a_1385_FindDistanceBetweenArrays {
 
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
-
-        int ans = 0;
+        int dist = 0;
         Arrays.sort(arr2);
-
-        for (int j = 0; j < arr1.length; j++) {
-            if (!inRange(arr2, arr1[j] - d, arr1[j] + d)) {
-                ans++;
+        for (int i = 0; i < arr1.length; i++) {
+            if (!inRange(arr2, arr1[i] - d, arr1[i] + d)) {
+                dist++;
             }
         }
-        return ans;
+        return dist;
     }
 
-    public boolean inRange(int[] arr, int min, int max) {
-        System.out.println("In range Left: min: " + min + ", max: " + max);
-        int mid = 0;
-        int left = 0;
-        int right = arr.length - 1;
-        while (left <= right && left < arr.length) {
+    public boolean inRange(int[] nums, int min, int max) {
 
-            mid = left + (right - left) / 2;
-            int val = arr[mid];
-            if (min <= val && val <= max) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= min && nums[mid] <= max) {
                 return true;
-            } else if (val > max) {
-                if (left == right) {
-                    return false;
-                }
-                right = mid - 1;
-            } else {
-                if (left == right) {
-                    return false;
-                }
-                left = mid + 1;
             }
-            System.out.println();
+
+            if (left == right) {
+                return false;
+            } else {
+                if (nums[mid] > max) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
         }
 
         return false;
